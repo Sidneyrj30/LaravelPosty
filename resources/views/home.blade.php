ca327b4b12a243d551bde102e52a238612718e8b
Sidney Rodrigues <sidneyrj45@gmail.com>
2022-02-21 17:06:30 -0300

    project posty

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class=" flex justify-center my-auto pt-40">
+    <div class="w-8/12 bg-white p-6 rounded-lg">
+    <p class="text-6xl text-center">Welcome to posty</p> 
+    </div>
+</div>
+
+
+@endsection
